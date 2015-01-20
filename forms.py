3336e025d0d68b9df217a88a7e52d26cdad7ee42
Eric Streeper <eric.streeper@gmail.com>
2015-01-20 03:50:32 -0800

    Begin implementing WTForms for adding quizzes and questions

---
+++ forms.py
@@ -0,0 +1,12 @@
+from wtforms import Form, TextField, TextAreaField
+
+class AddQuiz(Form):
+    title = TextField('Name of quiz', [validators.Length(max=60), 
+                                        validators.required()])
+    creator = TextField('Creator', [validators.Length(max=200)])
+
+class AddQuizQuestion(Form):
+    question = TextAreaField('Question', [validators.required(),
+                                        validators.Length(max=5000)])
+    answer = TextAreaField('Answer', [validators.required(),
+                                        validators.Length(max=5000)])
