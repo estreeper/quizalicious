b222fbbbcca019a1849e70cc46b1527fa5fe2082
Eric Streeper <eric.streeper@gmail.com>
2015-01-27 02:52:04 -0800

    Add function to get a question

--- database.py
+++ database.py
@@ -3,3 +3,7 @@ from redis import StrictRedis
 class QuizDB(StrictRedis):
     def get_all_quizzes(self):
         return self.smembers('quiz')
+
+    def get_question(self, quizid, questionid):
+        return self.hget("{0}:question".format(quizid), questionid)
+
