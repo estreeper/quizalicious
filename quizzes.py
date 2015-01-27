e3036332bbad61eb68e20d07eeafad03be2f2437
Eric Streeper <eric.streeper@gmail.com>
2015-01-27 02:01:28 -0800

    Created a new class to represent Quizzes
    Also started to add CRUD methods for cards/questions/answers.

---
+++ quizzes.py
@@ -0,0 +1,23 @@
+from database import QuizDB
+
+db = QuizDB(host=config.REDIS_HOST, port=config.REDIS_PORT)
+
+class Quiz(Base):
+    def __init__(self, id):
+        self.id = id
+
+    QUESTION_HASH = "{0}:question".format(self.id)
+    ANSWER_HASH = "{0}:answer".format(self.id)
+
+    def new_card(self, question, answer):
+
+        assert db.hlen(QUESTION_HASH) == db.hlen(ANSWER_HASH)
+
+        q_id = max([int(i) for i in db.hkeys(QUESTION_HASH)]) + 1
+        db.hset(QUESTION_HASH, q_id, question)
+        db.hset(ANSWER_HASH, q_id, answer)
+
+    def delete_card(self, q_id):
+        db.hdel(QUESTION_HASH, q_id)
+        db.hdel(ANSWER_HASH, q_id)
+
