ceb2d4f48dddd43867eea3b3a722e48c9ab30569
Eric Streeper <eric.streeper@gmail.com>
2015-02-24 01:32:24 -0800

    Start on update functions
    Set up score update functions, which can be used by future update
    algorithms. Ripe for refactoring, but it'll do for now.

--- quizalicious.py
+++ quizalicious.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from redis import StrictRedis
 from forms import AddQuizQuestion
+from probchoice import ProbabalisticChoice
 import random
 import config
 
@@ -9,6 +10,7 @@ app.debug = config.DEBUG
 app.secret_key = config.SECRET_KEY
 
 db = StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+random_chooser = ProbabalisticChoice()
 
 @app.route('/')
 def main():
@@ -25,12 +27,36 @@ def start_quiz(quiz_url_name):
 
 @app.route('/quiz/<quiz_url_name>')
 def quiz(quiz_url_name):
-    #FIXME: naive random implementation, needs to be score-weighted
     hash_name = "{0}:questions".format(quiz_url_name)
     all_questions = db.hkeys(hash_name)
-    question = random.choice(all_questions)
-    answer = db.hget(hash_name, question)
+
+    # Collect the information needed for probabalisticchoice function
+    #FIXME set up proper users rather than using the config file
+    #FIXME oh no, very very ugly! define somewhere else, get db logic out!
+    correct_sset = "{0}:correct:{1}".format(quiz_url_name, config.USERNAME)
+    incorrect_sset = "{0}:incorrect:{1}".format(quiz_url_name, config.USERNAME)
+    scores_sset = "{0}:scores:{1}".format(quiz_url_name, config.USERNAME)
+    probs_sset = "{0}:probs:{1}".format(quiz_url_name, config.USERNAME)
     quiz_name = db.hget(quiz_url_name, 'quiz_name')
+    # this will initialize the questions in the sorted set if they are not there or do nothing if they are
+    for q in all_questions:
+        db.zincrby(scores_sset, q, amount=0)
+
+    if request.args.get('answer') == 'correct':
+        db.zincrby(scores_sset, question, amount=1)
+    if request.args.get('answer') == 'incorrect':
+        db.zincrby(scores_sset, question, amount=-1)
+
+    # calculate the probabilities
+    #FIXME all this should be migrated out too
+    multiplier = config.MULTIPLIER
+
+    pairs = db.zrange(probs_sset, -1, -1, withscores=True)
+    question = random_chooser.probabalisticchoice(pairs)
+
+    answer = db.hget(hash_name, question)
+    
+
 
     return render_template('quiz.html', quiz_name=quiz_name,
             question=question, answer=answer, 
